2638970e300cc6e5def270ea4de04d15df3bf322
Marta Nelson <p3ngu1n33@msn.com>
2017-02-13 00:58:29 -0800

    working with murri pluggin

---
+++ js/mnelson.js
@@ -0,0 +1,10 @@
+
+
+var grid = new Muuri({
+  container: document.getElementsByClassName('grid')[0],
+  // Muuri does not convert a node list to array automatically
+  // so we have to do it manually.
+  items: [].slice.call(document.getElementsByClassName('item'))
+});
+
+$('muuri').dragEnabled(true);
