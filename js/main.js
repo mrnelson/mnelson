5ae6bb344a3ce7aa6eedea3eceb1eda5845fae0d
Marta Nelson <p3ngu1n33@msn.com>
2017-02-14 09:59:49 -0800

    adding murk to site
    it is working. Now I will be adding images to the boxes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-
-
 var grid = new Muuri({
   container: document.getElementsByClassName('grid')[0],
   // Muuri does not convert a node list to array automatically
   // so we have to do it manually.
   items: [].slice.call(document.getElementsByClassName('item'))
 });
-
-$('muuri').dragEnabled(true);
